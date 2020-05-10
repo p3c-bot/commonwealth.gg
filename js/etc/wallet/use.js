9f0dfa4833518636a93e27401c33242438a20808
VicK <57548498+CyberBounty@users.noreply.github.com>
2020-05-10 09:12:45 +0700

    update use.js
    Added Modal popup

--- js/etc/wallet/use.js
+++ js/etc/wallet/use.js
@@ -124,3 +124,29 @@ $('#copyMNButton').on('click', function (){
 $(".home").click(function(){
     window.location.href = "/";
 });
+
+// Get the modal popup QrCode
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
